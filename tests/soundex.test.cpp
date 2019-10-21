718f405bf9f578fc4a4eb06c637e6ee8f0a78d8b
Hannupekka Sormunen <hannupekka.sormunen@gmail.com>
2019-10-21 07:54:44 +0300

    Page 37, 2.9 unfinished

--- tests/soundex.test.cpp
+++ tests/soundex.test.cpp
@@ -27,7 +27,16 @@ BOOST_FIXTURE_TEST_SUITE(SoundexEncoding, SoundexFixture)
     }
 
     BOOST_AUTO_TEST_CASE(ReplacesConsonantsWithAppropriateDigits) {
+        //The book recommends to use only one assert per test case when test driving
+        //This rule is averted here to demonstrate the effectiveness of solution
         BOOST_REQUIRE_EQUAL(soundex.encode("Ab"), "A100");
+        BOOST_REQUIRE_EQUAL(soundex.encode("Ac"), "A200");
+        BOOST_REQUIRE_EQUAL(soundex.encode("Ad"), "A300");
+        BOOST_REQUIRE_EQUAL(soundex.encode("Ax"), "A200");
+    }
+
+    BOOST_AUTO_TEST_CASE(IgnoresNonAlphabetics) {
+        BOOST_REQUIRE_EQUAL(soundex.encode("A#"), "A000");
     }
 
 BOOST_AUTO_TEST_SUITE_END()
