6330a1ca495706b488258b94ccc310133bbe5aea
Hannupekka Sormunen <hannupekka.sormunen@gmail.com>
2019-11-15 07:03:59 +0200

    Final tests for stringUtils

--- tests/stringUtil.test.cpp
+++ tests/stringUtil.test.cpp
@@ -43,5 +43,18 @@ BOOST_FIXTURE_TEST_SUITE(stringUtil_head, stringUtilFixture)
     BOOST_AUTO_TEST_CASE(WorksWithNonAlphabetCharacters) {
         BOOST_REQUIRE_EQUAL(stringUtil::head("*est"), "*");
     }
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_FIXTURE_TEST_SUITE(stringUtil_tail, stringUtilFixture)
+
+    BOOST_AUTO_TEST_CASE(ReturnsStringWithoutFistCharacter) {
+        BOOST_REQUIRE_EQUAL(stringUtil::tail("Test"), "est");
+        BOOST_REQUIRE_EQUAL(stringUtil::tail("test"), "est");
+    }
+
+    BOOST_AUTO_TEST_CASE(WorksWithNonAlphabetCharacters) {
+        BOOST_REQUIRE_EQUAL(stringUtil::tail("T'st"), "'st");
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
